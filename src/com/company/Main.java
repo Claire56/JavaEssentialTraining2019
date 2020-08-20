a9234bb89d733cb440fd9a19a37df9ca41f27b2e
claire muwanguzi <kclaire56@yahoo.com>
2020-08-19 21:37:29 -0700

    added examples

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,15 +8,29 @@ public class Main {
 
         var sc = new Scanner(System.in);
 
-        System.out.print("Enter a numeric value: ");
-        var d1 = sc.nextDouble();
+        double d1 = getD1(sc, "Enter Numeric 1");
 
-        System.out.print("Enter a numeric value: ");
-        var d2 = sc.nextDouble();
+        double d2 = getD1(sc, "Enter Numeric 2");
 
         double result = d1 / d2;
-
         System.out.println("The answer is " + result);
+
+        var addResult = addNumbers(d1,d1,d2,d1,d2);
+        System.out.println("The result of adding " + d1 + "+" + d1 + "+" + d2+ "+" + d1 + "+" + d2 + " is :" + addResult);
+
+
+    }
+
+    private static double getD1(Scanner sc, String request) {
+        System.out.print(request);
+        return sc.nextDouble();
+    }
+    private static double addNumbers(double... values){
+        double result =0 ;
+        for(var value: values){
+            result += value;
+        }
+        return result;
     }
 
 }
