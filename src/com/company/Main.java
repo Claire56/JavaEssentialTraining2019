7e9671272de819ad8767ce8f7860d50c1e232bcb
claire muwanguzi <kclaire56@yahoo.com>
2020-08-20 08:10:52 -0700

    chapter 6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        String girl = null;
+        try {
+            String g = girl.substring(1,2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("finally");
+        }
+        System.out.println("we still running!");
     }
 }
